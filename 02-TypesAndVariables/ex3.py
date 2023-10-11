33378f93c44eff2f26d94c173c1653d977f8369d
demianvikovych <demvikovych@gmail.com>
2023-10-11 19:26:45 +0300

    My first Python programs from textbook

---
+++ 02-TypesAndVariables/ex3.py
@@ -0,0 +1,12 @@
+width = 17
+height = 12.0
+
+x1 = width//2 
+x2 = width/2
+x3 = height/3
+x4 = 1 + 2 * 5
+
+print("Case 1", type(x1))
+print("Case 2", type(x2))
+print("Case 3", type(x3))
+print("Case 4", type(x4))
---
+++ 02-TypesAndVariables/ex4.py
@@ -0,0 +1,5 @@
+celciusDegrees = float(input("Enter temperature in Celcius: "))
+
+farenheitDegrees = celciusDegrees * 1.8 + 32
+
+print("Temperature in farenheits:", farenheitDegrees)
